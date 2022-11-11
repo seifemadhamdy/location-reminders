7173a78d6b1d01e2bf350b7e2ce590813a9bd360
seifemadhamdy <101604949+seifemadhamdy@users.noreply.github.com>
2022-11-11 23:31:57 +0200

    Feature reminders navigation
    Implementation of reminders list hosting activity's navigation logic.
    
    Changelog:
    * Add:
    	The material toolbar defined in the associated layout file to be setup with the navigation component, and the navigation logic for when to close the activity on back key press, if the user is logged in, if not then the user is redirected to authenticate through AuthenticationActivity.

--- app/src/main/java/seifemadhamdy/locationreminders/locationreminders/RemindersActivity.kt
+++ app/src/main/java/seifemadhamdy/locationreminders/locationreminders/RemindersActivity.kt
@@ -1,21 +1,57 @@
 package seifemadhamdy.locationreminders.locationreminders
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.firebase.auth.FirebaseAuth
 import seifemadhamdy.locationreminders.R
+import seifemadhamdy.locationreminders.authentication.AuthenticationActivity
+import seifemadhamdy.locationreminders.databinding.ActivityRemindersBinding
+import seifemadhamdy.locationreminders.locationreminders.reminderslist.ReminderListFragment
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityRemindersBinding
     private val navController by lazy { (supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment).navController }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminders)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reminders)
+
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            setSupportActionBar(binding.materialToolbar)
+
+            NavigationUI.setupActionBarWithNavController(
+                this,
+                navController
+            )
+
+            onBackPressedDispatcher.addCallback(
+                this,
+                object : OnBackPressedCallback(true) {
+                    override fun handleOnBackPressed() {
+                        if (supportFragmentManager.findFragmentById(R.id.fragment_container_view)?.childFragmentManager?.fragments?.get(
+                                0
+                            ) is ReminderListFragment
+                        ) {
+                            finishAffinity()
+                        } else {
+                            navController.popBackStack()
+                        }
+                    }
+                })
+        } else {
+            // Redirect the unauthenticated user to log in
+            navigateToAuthenticationActivity()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -27,5 +63,20 @@ class RemindersActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            navController,
+            AppBarConfiguration.Builder(navController.graph).build()
+        )
+    }
+
+    private fun navigateToAuthenticationActivity() {
+        Intent(this, AuthenticationActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(this)
+            this@RemindersActivity.finish()
+        }
     }
 }
