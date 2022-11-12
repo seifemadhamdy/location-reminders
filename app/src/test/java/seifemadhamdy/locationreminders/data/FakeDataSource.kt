1e36af3af89c81fe0c5187dcf2e4bc428898130e
seifemadhamdy <101604949+seifemadhamdy@users.noreply.github.com>
2022-11-12 15:34:26 +0200

    Test FakeDataSource
    Implementation of a fake data source to act as a double to the real data source.
    
    Changelog:
    * Add:
    	Getting reminders returns the list of reminders if successful, and if not an error message is returned when reminders couldn't be found or error getting reminders when asked to return error (for simulation).
    	Saving reminder to the reminders list.
    	Getting a reminder by its id returns the reminder if successful, and if not an error message stating that the reminders couldn't be found is returned for both (simulated or not) cases.
    	Deleting all reminders by clearing the list of reminders.

--- app/src/test/java/seifemadhamdy/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/seifemadhamdy/locationreminders/data/FakeDataSource.kt
@@ -3,25 +3,67 @@ package seifemadhamdy.locationreminders.data
 import seifemadhamdy.locationreminders.locationreminders.data.ReminderDataSource
 import seifemadhamdy.locationreminders.locationreminders.data.dto.ReminderDTO
 import seifemadhamdy.locationreminders.locationreminders.data.dto.Result
+import kotlin.properties.Delegates
 
 // Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(private var remindersList: MutableList<ReminderDTO>? = mutableListOf()) :
+    ReminderDataSource {
 
-    // TODO: Create a fake data source to act as a double to the real data source
+    // Create a fake data source to act as a double to the real data source
+
+    private var shouldReturnError by Delegates.notNull<Boolean>()
+
+    // Used for controlling the behaviour of the functions in the FakeDataSource to simulate error case
+    fun setReturnError(shouldReturnError: Boolean = false) {
+        this.shouldReturnError = shouldReturnError
+    }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        // Return the reminders
+
+        // Return a simulated error
+        if (shouldReturnError) {
+            return Result.Error("Error getting reminders.")
+        }
+
+        // Return remindersList if successful
+        remindersList?.let {
+            return Result.Success(it)
+        }
+
+        // Return error if reminders couldn't be found
+        return Result.Error("Reminders couldn't be found.")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        // Save the Reminder
+        remindersList?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        // Return the reminder with the id
+
+        // Get the reminder by id from the list of available reminders
+        val reminder = remindersList?.find {
+            it.id == id
+        }
+
+        return when {
+            // Return a simulated error
+            shouldReturnError -> Result.Error("Reminders couldn't be found.")
+            // Return reminder if successful
+            reminder != null -> Result.Success(reminder)
+            // Return error that the reminders couldn't be found
+            else -> {
+                Result.Error("Reminders couldn't be found.")
+            }
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        // Delete all the reminders
+
+        // Clear the list of reminders in remindersList
+        remindersList?.clear()
     }
 }
